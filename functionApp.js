6a4f1e5f878f97eed3decdd9efdb8fc945839de7
Kok Yee Yang <yee_yang94@hotmail.com>
2026-03-17 15:14:47 +0800

    added more stuff for Azure functions app

--- functionApp.js
+++ functionApp.js
@@ -3,9 +3,11 @@ require("dotenv").config();
 const { app } = require("@azure/functions");
 const { run: runCandidateStateSync } = require("./src/index");
 const { run: runPlacementStatusSync } = require("./src/placementStatusSync");
+const { run: runClientCorporation360Sync } = require("./src/clientCorporation360Sync");
 
 const candidateSchedule = process.env.AZURE_CANDIDATE_SYNC_SCHEDULE || "0 0 2 * * *";
 const placementSchedule = process.env.AZURE_PLACEMENT_STATUS_SYNC_SCHEDULE || "0 */5 * * * *";
+const keyAccount360Schedule = process.env.AZURE_CORPORATION_360_SYNC_SCHEDULE || "0 */5 * * * *";
 
 app.timer("candidateStateSync", {
   schedule: candidateSchedule,
@@ -22,3 +24,11 @@ app.timer("placementStatusSync", {
     await runPlacementStatusSync();
   },
 });
+
+app.timer("clientCorporation360Sync", {
+  schedule: keyAccount360Schedule,
+  handler: async (_timer, context) => {
+    context.log("Running client corporation 360 sync");
+    await runClientCorporation360Sync();
+  }
+})
